fcb44dce947721e1355a2b7f710873d0f3d7c49f
xanthemc <39460939+xanthemc@users.noreply.github.com>
2023-02-07 00:44:21 +0800

    1 file modified

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -8,7 +8,16 @@ import { User } from './user';
 export class GameService {
   constructor(private http: HttpClient) {}
   user: User[] = [];
+  userScore: any = [];
 
+  addUserScore(score: any) {
+    return this.userScore.push(score);
+  }
+  getUserScore(user_id: any) {
+    return this.user.find((x) => x.id == user_id);
+
+    // return this.userScore;
+  }
   getSingleUser(user_id: string) {
     return this.http.get<string>(`https://reqres.in/api/users/${user_id}`);
   }
